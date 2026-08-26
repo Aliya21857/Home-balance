bdd3caf140385773eaa618ebc96f2536d2d5ff47
Aliya Sharipova <Aliya21857@users.noreply.github.com>
2026-08-26 17:56:01 +0300

    Redesign dashboard with warm editorial visuals

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import App from './App';
 import './styles.css';
 import './enhancements.css';
 import './task-checkbox.css';
+import './redesign.css';
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
